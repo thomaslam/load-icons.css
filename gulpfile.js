5ced3980424d55ef10714671e80b6fa91cc97e12
thomaslam <tuananhlam95@gmail.com>
2015-07-30 21:47:11 -0700

    add css sourcemap, min file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint'),
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass');
 
 gulp.task('lint', function() {
   return gulp.src('./*.js')
@@ -9,11 +14,30 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
+gulp.task('style', function() {
+  return gulp.src('load-icons.scss')
+        .pipe(sourcemaps.init())
+          .pipe(sass().on('error', sass.logError))
+          .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+          }))
+          .pipe(gulp.dest('./'))
+          .pipe(minifycss())
+          .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./'));
+});
+
 gulp.task('watch-lint', function() {
   gulp.watch('./*.js', ['lint']);
 });
 
-gulp.task('default', ['lint', 'watch-lint'], function() {
+gulp.task('watch-style', function() {
+  gulp.watch('load-icons.scss', ['style']);
+})
+
+gulp.task('default', ['lint', 'watch-lint', 'style', 'watch-style'], function() {
   browserSync.init({
     server: {
       baseDir: './'
